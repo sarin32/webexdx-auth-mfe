0f1154934e00c6ba1299740a2013914629491468
Sarin Alexander <execode02@gmail.com>
2025-04-27 13:16:48 +0530

    [CNF] port updates

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 4201,
+    port: 9001,
   },
   plugins: [react()],
   resolve: {
