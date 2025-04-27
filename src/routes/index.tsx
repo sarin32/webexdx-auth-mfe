6b2108b52d6bb040cb30892b675ae675f40b934d
Sarin Alexander <execode02@gmail.com>
2025-04-27 13:06:51 +0530

    [CNF] path updates

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,7 @@ import {Route, Routes} from 'react-router-dom';
 export function RootRoutes() {
   return (
     <Routes>
-      <Route path="/*" element={<HomeLayout />}></Route>
+      <Route path="/auth" element={<HomeLayout />}></Route>
     </Routes>
   );
 }
